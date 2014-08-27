4f893ffbf78ac3b8fb5e0c110c924addaba666b4
Ballance <ballance@gmail.com>
2014-08-27 17:08:37 -0400

    Signed-off-by: Ballance <ballance@gmail.com>

---
+++ QueueDoubleStacked.Test/QueueTester.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QueueDoubleStacked.Test
+{
+    /// <summary>
+    /// Test class for our double-stacked queue
+    /// </summary>
+    [TestClass]
+    public class QueueDoubleStackedTester
+    {
+        [TestMethod]
+        public void CreateAndEmptyBlankQueue()
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            EnqueAndDequeue(0);
+            stopwatch.Stop();
+
+            Debug.WriteLine("Completed in {0}ms", stopwatch.ElapsedMilliseconds);
+        }
+        [TestMethod]
+        public void EnqueAndDequeue1()
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            EnqueAndDequeue(1);
+            stopwatch.Stop();
+
+            Debug.WriteLine("Completed in {0}ms", stopwatch.ElapsedMilliseconds);
+        }
+
+        [TestMethod]
+        public void EnqueAndDequeue10()
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            EnqueAndDequeue(10);
+            stopwatch.Stop();
+
+            Debug.WriteLine("Completed in {0}ms", stopwatch.ElapsedMilliseconds);
+        }
+
+        [TestMethod]
+        public void EnqueAndDequeue100()
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            EnqueAndDequeue(100);
+            stopwatch.Stop();
+
+            Debug.WriteLine("Completed in {0}ms", stopwatch.ElapsedMilliseconds);
+        }
+
+        [TestMethod]
+        public void EnqueAndDequeue10000()
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            EnqueAndDequeue(10000);
+            stopwatch.Stop();
+
+            Debug.WriteLine("Completed in {0}ms", stopwatch.ElapsedMilliseconds);
+        }
+
+        [TestMethod]
+        public void EnqueAndDequeue100000()
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            EnqueAndDequeue(100000);
+            stopwatch.Stop();
+
+            Debug.WriteLine("Completed in {0}ms", stopwatch.ElapsedMilliseconds);
+        }
+
+        /// <summary>
+        /// Shared method to enqueue and dequeue a specified number of items
+        /// </summary>
+        /// <param name="numberOfItems"></param>
+        private static void EnqueAndDequeue(int numberOfItems)
+        {
+            var queueStack = new DoubleStackQueue();
+            Debug.WriteLine("Enqueueing {0} items...", numberOfItems);
+            for (int i = 1; i <= numberOfItems; i++)
+            {
+                queueStack.Enqueue(String.Format("Item {0}", i));
+            }
+
+            Debug.WriteLine("Completed enqueueing {0} items", numberOfItems);
+            Debug.WriteLine(string.Empty);
+            Debug.WriteLine("Dequeueing {0} items...", numberOfItems);
+            while (queueStack.Count > 0)
+            {
+                queueStack.Dequeue();
+            }
+
+            Debug.WriteLine("Completed dequeueing {0} items.", numberOfItems);
+        }
+    }
+}
---
+++ QueueDoubleStacked/DoubleStackQueue.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace QueueDoubleStacked
+{
+    /// <summary>
+    /// DoubleStackQueue is a standard FIFO queue implemented per my insane boss's requirements that internally the queue is handled as two stacks
+    /// </summary>
+    public class DoubleStackQueue
+    {
+        // Contents are stored in StackA during persistence between operations
+        private Stack<string> StackA = new Stack<string>();
+
+        // Items are moved to StackB for retrieving the oldest item from the bottom of the Stack.
+        private Stack<string> StackB = new Stack<string>();
+
+        public int Count
+        {
+            get { return StackA.Count; }
+        }
+
+        public void Enqueue(string item)
+        {
+            while (StackB.Count > 0)
+            {
+                StackA.Push(StackB.Pop());
+            }
+            StackA.Push(item);
+        }
+
+        public string Dequeue()
+        {
+            while (StackA.Count > 0)
+            {
+                StackB.Push(StackA.Pop());
+            }
+            var valueToReturn = StackB.Pop();
+            while (StackB.Count > 0)
+            {
+                StackA.Push(StackB.Pop());
+            }
+            return valueToReturn;
+        }
+
+
+    }
+}
